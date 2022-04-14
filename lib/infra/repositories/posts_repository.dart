a01f0b5a050a88e37b52115acd360e7ce6b4ff74
Gabriel B <gabrielbertollo.gb@gmail.com>
2022-04-14 15:52:02 -0300

    feat: implemented infra layer

---
+++ lib/infra/repositories/posts_repository.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_clean_cubit/domain/entities/post_entity.dart';
+import 'package:flutter_clean_cubit/domain/repositories/posts_repository_interface.dart';
+import 'package:flutter_clean_cubit/infra/datasources/posts_external_datasource_interface.dart';
+
+class PostsRepository implements IPostsRepository {
+  final IPostsExternalDatasource _postsExternalDatasource;
+
+  const PostsRepository({
+    required IPostsExternalDatasource postsExternalDatasource,
+  }) : _postsExternalDatasource = postsExternalDatasource;
+
+  @override
+  Future<List<PostEntity>> getPosts() async {
+    return await _postsExternalDatasource.getPosts();
+  }
+}
