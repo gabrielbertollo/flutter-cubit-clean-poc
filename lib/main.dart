4d18635a9c9c7cd16d5eb46ba152eac6f0344d55
Gabriel B <gabrielbertollo.gb@gmail.com>
2022-04-18 16:15:33 -0300

    feat: added create post route

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_clean_cubit/external/posts_external_datasource_mock.dart';
+import 'package:flutter_clean_cubit/presenter/pages/create_post/create_post_page.dart';
+import 'package:flutter_clean_cubit/presenter/pages/create_post/cubit/create_post_cubit.dart';
 import 'package:flutter_clean_cubit/presenter/pages/home/cubit/posts_cubit.dart';
 
 import 'presenter/pages/home/home_page.dart';
@@ -21,6 +23,12 @@ class MyApp extends StatelessWidget {
         ),
         child: const HomePage(),
       ),
+      routes: {
+        '/create_post': (context) => BlocProvider(
+              create: (context) => CreatePostCubit(),
+              child: const CreatePostPage(),
+            ),
+      },
     );
   }
 }
